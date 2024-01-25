b64eb05798c4592c0eb2a349a799bed205fa6953
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-01-25 10:15:21 -0500

    end of lesson 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
+import sourceData from '@/data.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,10 +11,34 @@ const router = createRouter({
       component: Home
     },
     {
+      path: '/protected',
+      name: 'protected',
+      component: ()=> import('@/views/ProtectedView.vue'),
+      meta:{
+        requiresAuth: true,
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: ()=> import('@/views/LoginView.vue'),
+    },
+    {
       path: '/destination/:id/:slug',
       name: 'destination.show',
       component: ()=> import('@/views/DestinationShow.vue'),
       props: route=>({...route.params, id: parseInt(route.params.id)}),
+      beforeEnter(to){
+        const exists = sourceData.destinations.find(
+          destination => destination.id === parseInt(to.params.id)
+        )
+        if(!exists) return {
+          name: 'NotFound',
+          params: { pathMatch: to.path.split('/').slice(1)},
+          query: to.query,
+          hash: to.hash,
+        }
+      },
       children:[
         {
           path: ':experienceSlug',
@@ -28,7 +53,16 @@ const router = createRouter({
       name: 'NotFound',
       component: ()=> import('@/views/NotFound.vue')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return savedPosition || new Promise((resolve)=>{
+      setTimeout(()=> resolve({ top:0 }), 300)
+    })
+  }
 })
-
+router.beforeEach((to)=>{
+  if(to.meta.requiresAuth && !window.user){
+    return {name: 'login'}
+  }
+})
 export default router
